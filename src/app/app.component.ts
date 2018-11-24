da424021897f6297f04b0d762713aab660d06371
Tom Howland <tomhowland9@gmail.com>
2018-11-24 18:59:37 +0000

    set up manual staging logic 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Stage } from 'src/enums/stage.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resistance-app';
+  Stage = Stage;
+  stage = Stage.Landing;
+
+  stageIsVisible(stage: Stage): boolean {
+    return this.stage === stage;
+  }
+
+  goToStage(stage: Stage) {
+    this.stage = stage;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LobbyPageComponent } from './lobby-page/lobby-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LobbyPageComponent
   ],
   imports: [
     BrowserModule,
