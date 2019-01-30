b502b29592bd2bedaa129d3d6f3dd8526f005199
Tom Howland <tomhowland9@gmail.com>
2019-01-30 22:41:00 +0000

    Make person icon more informative

--- src/app/session-info-bar/session-info-bar.component.ts
+++ src/app/session-info-bar/session-info-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Session } from 'src/models/session';
 import { Player } from 'src/models/player';
-import { teamPipe } from 'src/enums/team.enum';
+import { teamPipe, Team } from 'src/enums/team.enum';
 import { rolePipe } from 'src/enums/role.enum';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { BaseService } from 'src/services/base.service';
+import { bind } from 'src/functions';
 
 @Component({
   selector: 'app-session-info-bar',
@@ -12,10 +14,11 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 })
 export class SessionInfoBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _base: BaseService) { }
 
   @Input() session: Session;
   @Input() player: Player;
+  @Input() players: Player[];
 
   faUser = faUser;
 
@@ -24,7 +27,20 @@ export class SessionInfoBarComponent implements OnInit {
   }
 
   clickUser(): void {
-    alert(`You are a ${teamPipe(this.player.team)}: ${rolePipe(this.player.role)}`)
+    if(this.players === undefined){return ;}
+    if(this.player.team === Team.spy){ // TODO improve the click info
+      alert(`You are a ${teamPipe(this.player.team)}: ${rolePipe(this.player.role)}, and your fellow spies are ${this.fellowSpies}`)
+    } else {
+      alert(`You are a ${teamPipe(this.player.team)}: ${rolePipe(this.player.role)}`)
+    }
+  }
+
+  get fellowSpies(): string {
+    if(this.player.team !== Team.spy){ return 'none of your business'; }
+    return this.players
+      .filter((player) => player.team === Team.spy && player.name !== this.player.name)
+      .map((player) => player.name)
+      .join(', ');
   }
 
 }
