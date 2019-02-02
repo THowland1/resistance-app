0e609390c93a3634cfc5c063604961bc8a95b84f
Tom Howland <tomhowland9@gmail.com>
2019-02-02 17:16:17 +0000

    Move components into sub-folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { LobbyPageComponent } from './lobby-page/lobby-page.component';
-import { RoleRevealPageComponent } from './role-reveal-page/role-reveal-page.component';
+import { LobbyPageComponent } from './pages/lobby-page/lobby-page.component';
+import { RoleRevealPageComponent } from './pages/role-reveal-page/role-reveal-page.component';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatFormFieldModule, MatDialogModule, MatInputModule, MatCheckboxModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DevComponent } from './dev/dev.component';
-import { TeamPickPageComponent } from './team-pick-page/team-pick-page.component';
-import { VotePageComponent } from './vote-page/vote-page.component';
-import { MissionPageComponent } from './mission-page/mission-page.component';
-import { GameBoardComponent } from './game-board/game-board.component';
-import { GameOverPageComponent } from './game-over-page/game-over-page.component';
-import { SessionInfoBarComponent } from './session-info-bar/session-info-bar.component';
+import { DevComponent } from './components/dev/dev.component';
+import { TeamPickPageComponent } from './pages/team-pick-page/team-pick-page.component';
+import { VotePageComponent } from './pages/vote-page/vote-page.component';
+import { MissionPageComponent } from './pages/mission-page/mission-page.component';
+import { GameBoardComponent } from './components/game-board/game-board.component';
+import { GameOverPageComponent } from './pages/game-over-page/game-over-page.component';
+import { SessionInfoBarComponent } from './components/session-info-bar/session-info-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
